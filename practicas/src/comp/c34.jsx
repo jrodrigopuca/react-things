b6d228f1cf96ccf510217a169959bbfbae9b3034
J.A. Rodrigo Puca <r@geeks.ms>
2020-01-14 22:23:26 -0300

    1/14: Modal Part IV (working)

--- practicas/src/comp/c34.jsx
+++ practicas/src/comp/c34.jsx
@@ -1,9 +1,7 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Link, Route } from 'react-router-dom';
 
 function Modal(props) {
-
-    let contenido = props.contenido;
     let style = {
         position: "absolute",
         left: '10%',
@@ -12,44 +10,50 @@ function Modal(props) {
         color: '#f0ff00',
     }
 
-    /*
-    const click= ()=>{console.log("salir")};
-    <button onClick={click}>x</button>
-    */
-
     return (
         <div className="modal" style={style} onClick={props.onClick}>
             {props.children}
-            
         </div>
     )
 }
 
 function Colores(props) {
-    let color= "#"+props.match.params.id;
-    const style={
-        width:"100%",
-        height:200,
-        backgroundColor:color,
+
+    let color = "#" + props.match.params.id;
+    const style = {
+        width: "100%",
+        height: 200,
+        backgroundColor: color,
     }
     //props.history.push(props.match.url);
+
+    const changeShow = () => {
+        //show = false;
+        console.log(props.history);
+        props.history.goBack();
+    }
+
     return (
         <>
-        texto texto texto
-        <Modal contenido={color} onClick={()=>{console.log('hola')}}>
-            <div style={style}>
-                Mooooodal
+            texto texto texto
+            <li><Link to="/img/264796">Color 1</Link></li>
+            <li><Link to="/img/235796">Color 2</Link></li>
+            <li><Link to="/img/248796">Color 3</Link></li>
+            {!!props.match.params.id && <Modal contenido={color} onClick={changeShow}>
+                <div style={style}>
+                    Mooooodal
             </div>
-        </Modal>
+            </Modal>}
+
         </>
     )
 }
 
-function Home(){
-    return(
-    <ul>
-        <li><Link to="/img/ff00ff">Color 1</Link></li>
-    </ul>)
+function Home() {
+    return (
+        <ul>
+            <li><Link to="/img/">Ir a galeria de colores</Link></li>
+        </ul>)
 }
 
 
@@ -57,8 +61,10 @@ function C34() {
     return (
         <BrowserRouter>
             <Switch>
-            <Route exact path="/" children={Home}/>
-            <Route path="/img/:id" children={Colores} />
+                <Route exact path="/" children={Home} />
+                <Route path="/img/:id" children={Colores} />
+                <Route path="/img/" children={Colores} />
+
             </Switch>
         </BrowserRouter>
     )
